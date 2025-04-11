601b000f23857bbcd83d96c4fb1c3789fab2b9dc
John Otte <johnsmall407@gmail.com>
2025-04-11 09:51:36 -0500

    support class/function imports

--- src/replacer.py
+++ src/replacer.py
@@ -5,6 +5,11 @@ import yaml
 from pathlib import Path
 from typing import Callable
 from clipboard import Clip
+import importlib
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 @dataclass
 class Rule(ABC):
@@ -52,6 +57,49 @@ class StringMethodRule(Rule):
         
     def apply(self, text):
         return self.method(text)
+    
+
+@dataclass
+class ClassImportRule(Rule):
+    module: str
+    class_name: str
+    init: dict
+    method_name: str
+    method: Callable = None
+
+    def __post_init__(self):
+        module = importlib.import_module(self.module)
+        cls = getattr(module, self.class_name)
+        if self.init is None:
+            # This indicates a class method
+            self.method = getattr(cls, self.method_name, None)
+        else:
+            # Bound method
+            init_args = self.init.get("args", [])
+            init_kwargs = self.init.get("kwargs", {})
+            instance = cls(*init_args, **init_kwargs)
+            self.method = getattr(instance, self.method_name, None)
+        if not self.method:
+            raise ValueError(f"Invalid method: {self.method_name} in {self.class_name} of {self.module}")
+
+    def apply(self, text):
+        return self.method(text)
+    
+
+@dataclass
+class FunctionImportRule(Rule):
+    module: str
+    function_name: str
+    function: Callable = None
+
+    def __post_init__(self):
+        module = importlib.import_module(self.module)
+        self.function = getattr(module, self.function_name, None)
+        if not self.function:
+            raise ValueError(f"Invalid function: {self.function_name} in {self.module}")
+
+    def apply(self, text):
+        return self.function(text)
 
 
 class Replacer:
@@ -59,6 +107,8 @@ class Replacer:
         "regex": RegexRule,
         "replace": ReplaceRule,
         "str_method": StringMethodRule,
+        "class_method": ClassImportRule,
+        "function": FunctionImportRule,
     }
 
     def __init__(self, rules_path: str | Path = None):
@@ -86,7 +136,10 @@ class Replacer:
             rule_class = Replacer.RULES.get(rule_type)
             if not rule_class:
                 raise ValueError(f"Unknown rule type: {rule_type}")
-            compiled_rules.append(rule_class(**rule))
+            instance = rule_class(**rule)
+            compiled_rules.append(instance)
+            logger.info(f"Loaded rule: {instance.name} ({rule_type})")
+            logger.debug(f"Rule details: {instance}")
 
         return compiled_rules
     
